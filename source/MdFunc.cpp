phpmooc/AutoHotkey

commit 002df24cb0398606c8a8d2b24d5d834830cf1aa1

Changed GetOwnPropDesc to return classes for numeric types.

source/MdFunc.cpp
@@ -420,8 +420,14 @@ bool MdFunc::Call(ResultToken &aResultToken, ExprTokenType *aParam[], int aParam
 			aborted = (res == FR_ABORTED);
 		break;
 	case MdType::ResultType: aResultToken.SetResult((ResultType)rup); break;
+#ifndef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::Int32: aResultToken.SetValue(ri32); break;
 	case MdType::UInt64:
+#ifdef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::Int64: aResultToken.SetValue(ri64); break;
 	case MdType::UInt32: aResultToken.SetValue((UINT)rup); break;
 	//case MdType::Float64: aResultToken.SetValue(GetDoubleRetval()); break;
@@ -555,8 +561,14 @@ void TypedPtrToToken(MdType aType, void *aPtr, ExprTokenType &aToken)
 	switch (aType)
 	{
 	case MdType::Bool32:
+#ifndef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::Int32: aToken.SetValue(*(int*)aPtr); break;
 	case MdType::UInt32: aToken.SetValue(*(UINT*)aPtr); break;
+#ifdef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::UInt64:
 	case MdType::Int64: aToken.SetValue(*(__int64*)aPtr); break;
 	case MdType::Float64: aToken.SetValue(*(double*)aPtr); break;
@@ -586,8 +598,14 @@ ResultType SetValueOfTypeAtPtr(MdType aType, void *aPtr, ExprTokenType &aValue, 
 	switch (aType)
 	{
 	//case MdType::Bool32:
+#ifndef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::Int32:
 	case MdType::UInt32: *(UINT*)aPtr = (UINT)nt.value_int64; break;
+#ifdef _WIN64
+	case MdType::IntPtr:
+#endif
 	case MdType::UInt64:
 	case MdType::Int64: *(__int64*)aPtr = nt.value_int64; break;
 	case MdType::Float64: *(double*)aPtr = nt.symbol == SYM_FLOAT ? nt.value_double : (double)nt.value_int64; break;
@@ -618,6 +636,7 @@ size_t TypeSize(MdType aType)
 	case MdType::Float64:
 	case MdType::Int64:
 	case MdType::UInt64: return 8;
+	case MdType::IntPtr: return sizeof(void*);
 	default: return 0;
 	}
 }

source/MdType.h
@@ -17,8 +17,9 @@ enum class MdType : UINT8
 	UInt32		= 6,
 	Int64		= 7,
 	UInt64		= 8,
-	Float64		= 9,
-	Float32		= 10,
+	IntPtr		= 9,
+	Float64		= 10,
+	Float32		= 11,
 	String,
 	Object,
 	Variant, // Currently only for input (ExprTokenType) or retval (ResultToken).
@@ -46,7 +47,7 @@ enum class MdType : UINT8
 	FirstModifier = Optional,
 	BitsUpperBound = Optional,
 	UIntPtr = Exp32or64(UInt32, UInt64),
-	IntPtr = Exp32or64(Int32, Int64)
+	LastSupportedPropertyType = Float32
 };
 
 #define MDTYPE_NAMES nullptr, _T("i8"), _T("u8"), _T("i16"), _T("u16"), _T("i32"), _T("u32"), _T("i64"), _T("u64"), _T("f64"), _T("f32")
@@ -78,6 +79,7 @@ template<> struct md_argtype<MdType::Float32> { typedef float t; };
 template<> struct md_argtype<MdType::Float64> { typedef double t; };
 template<> struct md_argtype<MdType::Int64> { typedef __int64 t; };
 template<> struct md_argtype<MdType::UInt64> { typedef UINT64 t; };
+template<> struct md_argtype<MdType::IntPtr> { typedef INT_PTR t; };
 template<> struct md_argtype<MdType::String> { typedef StrArg t; };
 template<> struct md_argtype<MdType::Object> { typedef IObject *t; };
 template<> struct md_argtype<MdType::Void> { typedef void t; };
@@ -104,6 +106,7 @@ template<> struct md_retval<MdType::Int32> { typedef int t; };
 template<> struct md_retval<MdType::UInt32> { typedef UINT t; };
 template<> struct md_retval<MdType::Int64> { typedef __int64 t; };
 template<> struct md_retval<MdType::UInt64> { typedef UINT64 t; };
+template<> struct md_retval<MdType::IntPtr> { typedef INT_PTR t; };
 template<> struct md_retval<MdType::Bool32> { typedef BOOL t; };
 
 template<> struct md_retval<MdType::FResult> { typedef FResult t; };

source/script_object.cpp
@@ -2204,7 +2204,7 @@ void Object::GetOwnPropDesc(ResultToken &aResultToken, int aID, int aFlags, Expr
 		if (field->tprop->class_object)
 			desc->SetOwnProp(_T("Type"), field->tprop->class_object);
 		else if (field->tprop->type != MdType::Void)
-			desc->SetOwnProp(_T("Type"), TypeName(field->tprop->type));
+			desc->SetOwnProp(_T("Type"), sPrimitiveClass[(int)field->tprop->type-1]);
 		else
 			desc->SetOwnProp(_T("Type"), (__int64)field->tprop->item_count);
 		desc->SetOwnProp(_T("Offset"), field->tprop->data_offset);
@@ -4249,7 +4249,7 @@ void Object::CreateRootPrototypes()
 	sCArrayPrototype->mFlags &= ~NativeClassPrototype;
 	sStructClass->DefineClass(_T("Array"), sCArrayClass, true);
 
-	LPTSTR const type_names[]{ _T("Float32"), _T("Float64"), _T("Int16"), _T("Int32"), _T("Int64"), _T("Int8"), _T("IntPtr"), _T("UInt16"), _T("UInt32"), _T("UInt8")};
+	LPTSTR const type_names[]{ _T("Float32"), _T("Float64"), _T("Int16"), _T("Int32"), _T("Int64"), _T("Int8"), _T("IntPtr"), _T("UInt16"), _T("UInt32"), _T("UInt8") };
 	MdType const type_codes[]{ MdType::Float32, MdType::Float64, MdType::Int16, MdType::Int32, MdType::Int64, MdType::Int8, MdType::IntPtr, MdType::UInt16, MdType::UInt32, MdType::UInt8 };
 	UCHAR const type_dllcall[]{ DLL_ARG_FLOAT, DLL_ARG_DOUBLE, DLL_ARG_SHORT, DLL_ARG_INT, DLL_ARG_INT64, DLL_ARG_CHAR, Exp32or64(DLL_ARG_INT,DLL_ARG_INT64), DLL_ARG_SHORT, DLL_ARG_INT, DLL_ARG_CHAR};
 	for (int i = 0; i < _countof(type_names); ++i)
@@ -4269,6 +4269,7 @@ void Object::CreateRootPrototypes()
 		tp->data_offset = 0;
 		auto c = CreateClass(type_names[i], sStructClass, p, nullptr);
 		CreatePtrClass(c, p, si);
+		sPrimitiveClass[(int)type_codes[i] - 1] = c;
 	}
 
 	GuiControlType::DefineControlClasses();
@@ -4291,6 +4292,7 @@ Object *Map::sPrototype;
 
 Object *Object::sClass;
 Object *Object::sStructClass, *Object::sPtrClass, *Object::sCArrayClass;
+Object *Object::sPrimitiveClass[(int)MdType::LastSupportedPropertyType];
 
 Object *Closure::sPrototype;
 Object *BoundFunc::sPrototype;

source/script_object.h
@@ -587,6 +587,7 @@ public:
 	
 	static ObjectMember sStructMembers[], sCArrayMembers[];
 	static Object *sStructClass, *sStructPrototype, *sPtrClass, *sPtrPrototype, *sCArrayClass, *sCArrayPrototype;
+	static Object *sPrimitiveClass[(int)MdType::LastSupportedPropertyType];
 
 	static void CreateRootPrototypes();
 	static Object *CreateClass(Object *aPrototype, Object *aBase = Object::sClassPrototype);

commit 4fe27c30e463350f26716119d65bbfd1c8081121

Fix f32 and f64
Broken by 002df24c

source/MdFunc.cpp
@@ -650,8 +650,6 @@ MdType TypeCode(LPCTSTR aName)
 	for (int i = 1; i < _countof(sTypeNames); ++i)
 		if (!_tcsicmp(sTypeNames[i], aName))
 			return (MdType)i;
-	if (!_tcsicmp(_T("iptr"), aName))
-		return MdType::IntPtr;
 	if (!_tcsicmp(_T("uptr"), aName))
 		return MdType::UIntPtr;
 	return MdType::Void;

source/MdType.h
@@ -50,7 +50,7 @@ enum class MdType : UINT8
 	LastSupportedPropertyType = Float32
 };
 
-#define MDTYPE_NAMES nullptr, _T("i8"), _T("u8"), _T("i16"), _T("u16"), _T("i32"), _T("u32"), _T("i64"), _T("u64"), _T("f64"), _T("f32")
+#define MDTYPE_NAMES nullptr, _T("i8"), _T("u8"), _T("i16"), _T("u16"), _T("i32"), _T("u32"), _T("i64"), _T("u64"), _T("iptr"), _T("f64"), _T("f32")
 
 #define MdType_IsInt(t) ((t) <= MdType::LastIntType && (t) >= MdType::FirstIntType)
 #define MdType_IsNum(t) ((t) <= MdType::LastNumberType && (t) >= MdType::FirstNumberType)